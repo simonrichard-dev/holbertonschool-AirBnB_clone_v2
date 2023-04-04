81838a5e11f0b38bc5eb60911dc7402a3b5118f6
simonrichard-dev <simon.insa@gmail.com>
2023-04-04 17:04:42 +0200

    add initial 2-c_route.py file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Comment """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello0():
+    """ function that return Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello1():
+    """ function that return HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/is_fun", strict_slashes=False)
+def hello2():
+    """ function that return C is fun"""
+    return "C is fun"
+
+
+@app.route("/c/cool", strict_slashes=False)
+def hello3():
+    """ function that return C cool"""
+    return "C cool"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
